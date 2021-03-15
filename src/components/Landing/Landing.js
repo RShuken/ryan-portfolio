a3b6125383faf130efabff5132bef6f63d7d8635
rshuken <ryanshuken@gmail.com>
2021-03-14 23:11:43 -0600

    new photo

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import headShot from '../../images/headshot.JPG';
+import headShot from '../../images/ryanheadshot.png';
 import './Landing.css';
 
 class Landing extends Component {
@@ -16,7 +16,7 @@ class Landing extends Component {
             <div className='designBox1'></div>
             <div className='designBox2'></div>
             <h1>Hello I'm Ryan Shuken</h1>
-            <img src={headShot} alt='headshot' />
+            <img src={headShot} alt='headshot' className="headshot_image" />
           </div>
           <h1 className='workTitle'>Full Stack Developer</h1>
         </section>
